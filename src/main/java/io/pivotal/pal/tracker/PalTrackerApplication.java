fd66371d252c51c338379a35c73c8f09b3f50fda
Andrew and Wisa <wisa_terhune-praphruettam+andrew_miller@bcbsil.com>
2019-12-11 08:32:03 -0700

    set up jdbc for time entry app

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,11 +1,17 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
@@ -13,7 +19,7 @@ public class PalTrackerApplication {
 
     @Bean
     public TimeEntryRepository getRepo() {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
 }
